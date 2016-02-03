5d6ce55b0b4865f126fa7a1a3ae56f1aa1a7ea47
Sergio Castillo <s.cast.lara@gmail.com>
2016-02-03 23:09:07 +0100

    Comments

--- factorial.go
+++ factorial.go
@@ -4,6 +4,7 @@ import "fmt"
 import "os"
 import "strconv"
 
+// (args) (returns)
 func factorial(n int)(int) {
     if n == 0 {
         return 1
@@ -13,7 +14,7 @@ func factorial(n int)(int) {
 }
 
 func main() {
-    num, _ := strconv.Atoi(os.Args[1])
+    num, _ := strconv.Atoi(os.Args[1]) // converts string to int
     var result int = factorial(num)
     fmt.Println("Factorial of", num, ":", result)
 }
